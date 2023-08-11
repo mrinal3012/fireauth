f418b88f71e2385e65d6cfe0505aed865e8764b6
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-08-11 23:53:36 +0600

    add card page

---
+++ lib/payment/second_card_done_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SecondCardDonePage extends StatefulWidget {
+  const SecondCardDonePage({Key? key}) : super(key: key);
+
+  @override
+  State<SecondCardDonePage> createState() => _SecondCardDonePageState();
+}
+
+class _SecondCardDonePageState extends State<SecondCardDonePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
