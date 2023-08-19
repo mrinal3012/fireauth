8f96d7dc1b270aef3b5151c5311481e097bd9784
mrinal3012 <mrinalpalit.cse@gmail.com>
2023-08-19 23:32:38 +0600

    add cancle page

---
+++ lib/history/cancle_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CanclePage extends StatefulWidget {
+  const CanclePage({Key? key}) : super(key: key);
+
+  @override
+  State<CanclePage> createState() => _CanclePageState();
+}
+
+class _CanclePageState extends State<CanclePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
